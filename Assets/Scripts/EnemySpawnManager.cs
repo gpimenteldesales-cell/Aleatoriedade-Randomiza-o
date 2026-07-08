4d25a60eca145e4b778180a145679f5452bc9f8e
GUIMê <gpimenteldesales@gmail.com>
2026-07-08 16:58:21 -0300

    CommitCoresAleatórias

--- Assets/Scripts/EnemySpawnManager.cs
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -4,14 +4,16 @@ public class EnemySpawnManager : MonoBehaviour
 {
     public Transform[] spawnPoints;
     public Transform[] spawnItem;
-    public GameObject enemyPrefab;
-    public GameObject itemPrefab;
-    public int randomSpawn;
+    public GameObject playerPrefab;
+    public GameObject[] itemPrefab;
+    public int random;
+    public int randomItem;
     void Start()
     {
-        randomSpawn = Random.Range(0,4);
-        Instantiate(enemyPrefab, spawnPoints[randomSpawn]);
-        Instantiate (itemPrefab[randomSpawn], spawnItem[randomSpawn]);
+        random = Random.Range(0,4);
+        Instantiate(playerPrefab, spawnPoints[random]);
+        randomItem = Random.Range(0,3);
+        Instantiate (itemPrefab[random], spawnItem[random]);
     }
 
     void Update()
